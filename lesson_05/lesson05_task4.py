007a761af8dafcc399ba30c91f01adb0831427cf
Zainullin-Bulat <bjkzn@yandex.ru>
2025-09-26 09:28:57 +0300

    commit

--- lesson_05/lesson05_task4.py
+++ lesson_05/lesson05_task4.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Edge()
+driver = webdriver.Firefox()
 driver.get("http://the-internet.herokuapp.com/login")
 
 driver.find_element(By.ID, "username").send_keys("tomsmith")
