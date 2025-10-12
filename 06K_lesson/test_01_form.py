161888562f65ef7aff6870a8e04a8a92a13fe969
Zainullin-Bulat <bjkzn@yandex.ru>
2025-10-12 11:11:09 +0300

    commit

--- 06K_lesson/test_01_form.py
+++ 06K_lesson/test_01_form.py
@@ -1,29 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Edge()
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
-driver.find_element(By.NAME, "first-name").send_keys("Иван")
-driver.find_element(By.NAME, "last-name").send_keys("Петров")
-driver.find_element(By.NAME, "address").send_keys("Ленина, 55-3")
-driver.find_element(By.NAME, "e-mail").send_keys("test@skypro.com")
-driver.find_element(By.NAME, "phone").send_keys("+7985899998787")
-driver.find_element(By.NAME, "city").send_keys("Москва")
-driver.find_element(By.NAME, "country").send_keys("Россия")
-driver.find_element(By.NAME, "job-position").send_keys("QA")
-driver.find_element(By.NAME, "company").send_keys("SkyPro")
+def test_01_form():
+    driver = webdriver.Edge()
+    driver.get("https://bonigarcia.dev/"
+               "selenium-webdriver-java/data-types.html")
 
-driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+    driver.find_element(By.NAME, "first-name").send_keys("Иван")
+    driver.find_element(By.NAME, "last-name").send_keys("Петров")
+    driver.find_element(By.NAME, "address").send_keys("Ленина, 55-3")
+    driver.find_element(By.NAME, "e-mail").send_keys("test@skypro.com")
+    driver.find_element(By.NAME, "phone").send_keys("+7985899998787")
+    driver.find_element(By.NAME, "city").send_keys("Москва")
+    driver.find_element(By.NAME, "country").send_keys("Россия")
+    driver.find_element(By.NAME, "job-position").send_keys("QA")
+    driver.find_element(By.NAME, "company").send_keys("SkyPro")
 
-zip_code = driver.find_element(By.ID, "zip-code")
-assert "alert py-2 alert-danger" in zip_code.get_attribute("class")
+    driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
-fields = ["first-name", "last-name", "address", "e-mail", "phone", "city",
-          "country", "job-position", "company"]
-for field in fields:
-    element = driver.find_element(By.ID, field)
-    assert "alert py-2 alert-success" in element.get_attribute("class")
+    zip_code = driver.find_element(By.ID, "zip-code")
+    assert "alert py-2 alert-danger" in zip_code.get_attribute("class")
 
-driver.quit()
-print("Проверка пройдена")
+    fields = ["first-name", "last-name", "address", "e-mail", "phone", "city",
+              "country", "job-position", "company"]
+    for field in fields:
+        element = driver.find_element(By.ID, field)
+        assert "alert py-2 alert-success" in element.get_attribute("class")
+
+    driver.quit()
+    print("Проверка пройдена")
--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -3,24 +3,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome()
-driver.get(
-    "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
-driver.find_element(By.CSS_SELECTOR, "#delay").clear()
-driver.find_element(By.CSS_SELECTOR, "#delay").send_keys("45")
+def test_02_calc():
+    driver = webdriver.Chrome()
+    driver.get(
+           "https://bonigarcia.dev/"
+           "selenium-webdriver-java/slow-calculator.html")
 
-driver.find_element(By.XPATH, "//span[text()='7']").click()
-driver.find_element(By.XPATH, "//span[text()='+']").click()
-driver.find_element(By.XPATH, "//span[text()='8']").click()
-driver.find_element(By.XPATH, "//span[text()='=']").click()
+    driver.find_element(By.CSS_SELECTOR, "#delay").clear()
+    driver.find_element(By.CSS_SELECTOR, "#delay").send_keys("45")
 
-WebDriverWait(driver, 46).until(
-    EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
-)
+    driver.find_element(By.XPATH, "//span[text()='7']").click()
+    driver.find_element(By.XPATH, "//span[text()='+']").click()
+    driver.find_element(By.XPATH, "//span[text()='8']").click()
+    driver.find_element(By.XPATH, "//span[text()='=']").click()
 
-result = driver.find_element(By.CSS_SELECTOR, ".screen").text
-assert result == "15"
+    WebDriverWait(driver, 46).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
+    )
 
-driver.quit()
-print("Проверка пройдена")
+    result = driver.find_element(By.CSS_SELECTOR, ".screen").text
+    assert result == "15"
+
+    driver.quit()
+    print("Проверка пройдена")
