a96e574b238ebe95ab21a9022c43e2e78ddab7d0
Zainullin-Bulat <bjkzn@yandex.ru>
2025-10-22 23:21:44 +0300

    commit

--- 07_lesson/shop/login_page.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-from base_page import BasePage
-
-
-class LoginPage(BasePage):
-    def login(self, username, password):
-        self.driver.find_element(By.ID, "user-name").send_keys(username)
-        self.driver.find_element(By.ID, "password").send_keys(password)
-        self.driver.find_element(By.ID, "login-button").click()
